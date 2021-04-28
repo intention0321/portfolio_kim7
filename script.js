25a1d64e0a0854018927c9918140b933113f30a9
intention0321 <76766433+intention0321@users.noreply.github.com>
2021-04-28 18:33:31 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+const menu_btn= document.querySelector('.menu_btn');
+const menu= document.querySelector('.menu');
 
-$(window).load(function(){
-    $('.loader').fadeOut(); 
+menu_btn.addEventListener('click',()=>{ 
+    menu.classList.toggle('active');
 });
 
+const scrollTrigger = new ScrollTrigger.default();
+
+scrollTrigger.add('[data-scroll]', {
+    toggle: {
+        class: {
+            in: 'animateIn',
+            out: 'animateOut'
+        }
+    }
+});
